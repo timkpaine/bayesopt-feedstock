cc0cc7e48ae687cacab1d4f36c9567fae4678de1
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2026-04-03 13:30:22 -0400

    Stop regenerating Cython sources in py build

--- recipe/build-py.sh
+++ recipe/build-py.sh
@@ -1,10 +1,5 @@
 export CXXFLAGS="${CXXFLAGS} -DBOOST_TIMER_ENABLE_DEPRECATED=1"
 
-# Cython 3 rejects np.int_t in this legacy source; use np.intp_t for compatibility.
-sed -i 's/np\.int_t/np.intp_t/g' python/bayesopt.pyx
-rm -f python/bayesopt.cpp
-cython python/bayesopt.pyx --cplus
-
 cd build
 cmake ${CMAKE_ARGS} \
   -DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
